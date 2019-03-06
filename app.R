c5c32e2b5808b1f75076d97365cba2b7e77329db
future_plant <hodde.j@gmail.com>
2019-03-06 20:39:30 +0100

    only show points that are not swimming pools

--- app.R
+++ app.R
@@ -9,21 +9,46 @@ source("ui.R")
 source("scripts/getpoints.R")
 source("scripts/geturls.R")
 source("scripts/hyperlinks.R")
+source("scripts/readgsheets.R")
 
 
 
 # server serves information to the user interface that is constructed in scripts/ui.R file
 server <- function(input, output, session) {
   
+  samples <- getGsheet()
+  waterpoints <- getPoints()
+  
+  # get rd of this line if it doesn't work
+  waterpoints = waterpoints[!waterpoints$OBJECTID %in% samples,]
+  
+  getColor <- function(samples,waterpoints) {
+    sapply(waterpoints$OBJECTID, function(OBJECTID) {
+      if(OBJECTID %in% samples) {
+        "red"
+      } else {
+        "blue"
+      } 
+      })
+  }
+  
+  icons <- awesomeIcons(
+    icon = 'ios-close',
+    iconColor = 'black',
+    library = 'ion',
+    markerColor = getColor(samples,waterpoints)
+  )
+  
   # Render background map for shiny app
   output$map <- renderLeaflet({
-    waterpoints <- getPoints()
+    
     waterlinks <- createFormLinks(waterpoints)
     # Add default OpenStreetMap map tiles
     leaflet(data = waterlinks) %>% setView(lng = 1.8243883, lat = 41.728416, zoom = 13) %>%  
       addTiles() %>% 
-      addMarkers( 
-                 popup =  ~formURL
+      addAwesomeMarkers(
+                  icon = icons,
+                  popup =  ~formURL
                  )
     
     })
---
+++ scripts/readgsheets.R
@@ -0,0 +1,16 @@
+# This script reads a google sheet and returns it as a dataframe
+
+library(gsheet)
+getGsheet <- function(){
+  # url of gsheet which contains form answers
+  url <- 'https://docs.google.com/spreadsheets/d/1rHUTv2m6N1cmx4gP0zwKxRBQok9Zr-FEBmf90Ksp7eU'
+  
+  # creates dataframe from google sheet answers
+  dtf <- read.csv(text=gsheet2text(url, format='csv'), stringsAsFactors=FALSE)
+  
+  # returns point ID's of points that have been classified as not suitable
+  return(dtf[which(dtf$El.punt.d.aigua.és.adequat. == 'No (piscina, ciment, edifici...)'),2])
+}
+
+
+      
